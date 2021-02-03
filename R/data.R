9623cf7f7ddb7dde6529622ef4ecc3f84db8742a
Zahier Nasrudin <zahiernasrudin@gmail.com>
2021-02-03 17:40:54 +0800

    Adding Malay stopwords list

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' @importFrom tibble tibble
+NULL
+#' Malay Stop Words.
+#'
+#' Malay Stop Words List from Abdullah, M. T. (2005). Improvement of Malay information retrieval using local stop words. Proc. IATC, 2005, 1-7.
+#'
+#' @format A data frame with one variable: \code{stopwords}
+"malaystopwords"
+
+
---
+++ data-raw/malaystopwords.R
@@ -0,0 +1,3 @@
+## code to prepare `malaystopwords` dataset goes here
+
+usethis::use_data(malaystopwords, overwrite = TRUE)
