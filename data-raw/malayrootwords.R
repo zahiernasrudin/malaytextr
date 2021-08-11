1c0242508bf5f9a8930612e3d0615bfebd68659b
Zahier Nasrudin <zahiernasrudin@gmail.com>
2021-08-11 14:00:46 +0800

    Update root words

--- data-raw/malayrootwords.R
+++ data-raw/malayrootwords.R
@@ -1,6 +1,11 @@
 ## code to prepare `malayrootwords` dataset goes here
 
 library(readr)
+library(dplyr)
+
 malayrootwords <- read_csv("G:/My Drive/1. Zahier Projects/13. malaytextr/data-raw/malayrootwords.csv")
+
+malayrootwords %>%
+  count(`Col Word`, sort = T)
 
 usethis::use_data(malayrootwords, overwrite = TRUE)
